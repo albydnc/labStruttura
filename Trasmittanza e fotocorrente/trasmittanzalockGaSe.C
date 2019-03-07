14ed40f2e8914cd7a44683389e08a992cfc335bc
sbrindignigni <46198453+sbrindignigni@users.noreply.github.com>
2019-03-07 17:32:49 +0100

    Migliorato

--- Trasmittanza e fotocorrente/trasmittanzalockGaSe.C
+++ Trasmittanza e fotocorrente/trasmittanzalockGaSe.C
@@ -18,15 +18,17 @@ void trasmittanzalockGaSe()
 
   // numero misure prese
   const int n = 18;
-  double trasm[]={92,194,214,200,3472,4350,5834,7213,8231,9027,9526,10160,10500,10748,11057,11128,11401,11751};
+  double trasm[]={9,16,17,16,275,344,460,567,646,706,744,791,815,835,860,866,890,920};
   double manopola[]={600,610,630,640,642,643,644,645,646,647,648,649,650,651,652,653,660,680};
-  double st[]={60,117,122,189,85,99,50,113,71,45,46,47,105,50,87,110,79,128};
+  //double st[]={60,117,122,189,85,99,50,113,71,45,46,47,105,50,87,110,79,128};
+  double st[n];
   double sm[n];
   double lambda[n];
   double sl[n];
   double a[]={-3.63456e+001,9.92450e-001};
  double sa[]={5.38479,1.08634e-002};
 for(int j=0;j<n;j++){
+   st[j]=1;
    lambda[j]=a[0]+a[1]*manopola[j];
    sm[j]=0.02;
    sl[j]=pow(sa[0]*sa[0]+manopola[j]*sa[1]*manopola[j]*sa[1]+a[1]*sm[j]*a[1]*sm[j],0.5);
@@ -45,21 +47,21 @@ for(int j=0;j<n;j++){
   giV->SetMarkerSize(0.6);
   giV->SetMarkerStyle(21);
   // Facile, titolo del grafico
-  giV->SetTitle("#lambda(pixel)");
+  giV->SetTitle("Trasmittanza GaSe Lock-In");
   // Titoli degli assi
-  giV->GetXaxis()->SetTitle("intensita");
+  giV->GetXaxis()->SetTitle("#lambda [nm]");
   //giV->GetXaxis()->SetAxisLimits(0,3000);
-  giV->GetYaxis()->SetTitle("#lambda [nm]");
+  giV->GetYaxis()->SetTitle("Trasm.Rel");
   // Do istruzioni al grafico di disegnarsi sul canvas che ho selezionato preventivamente con cd()
   // Esistono diverse opzioni di disegno, vedi anche https://root.cern.ch/doc/master/classTGraphPainter.html
   // "AP" è molto semplice, gli stiamo chiedendo di disegnare gli assi (A) e i punti (P)
   giV->Draw("AP");
-  TF1 *funz0 = new TF1("funz0","[0]+[1]/(1+exp(-(x-[2])/[3]))",100,800);
+  TF1 *funz0 = new TF1("funz0","[0]+[1]/(1+exp((x-[2])/[3]))",100,800);
   funz0->SetLineStyle(1);
   funz0->SetLineColor(2);
-  funz0->SetParameter(0,12000);
-  funz0->SetParameter(1,1);
-  funz0->SetParameter(2,590);
+  funz0->SetParameter(0,1000);
+  //funz0->SetParameter(1,1);
+  funz0->SetParameter(2,600);
   //funz0->SetParameter(3,4);
   giV->Fit(funz0,"RM+");
   cout << "Chi^2:" << funz0->GetChisquare() << ", number of DoF: " << funz0->GetNDF() << " (Probability: " << funz0->GetProb() << ")." << endl;
